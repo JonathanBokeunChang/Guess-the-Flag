65e3b3864f00524c3c27a9ad6ef296fb81add547
Jonathan Chang <jonathanbokeunchang@gmail.com>
2023-12-03 14:08:55 -0500

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Jonathan Chang on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
